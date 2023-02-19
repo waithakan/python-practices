997a07a1ddbfc1d850e71f9640b9a91c2a6266da
waithakan <waithakadylan@gmail.com>
2023-02-19 22:59:12 +0300

    more

---
+++ liiist.py
@@ -0,0 +1,4 @@
+#Write a Python program to display the first and last colors from the following list
+#of colors: red, orange, yellow, green, cyan, blue, magenta, and white.
+color_list = ["Red","Green","White","Black"]
+print(color_list[0], color_list[3])
